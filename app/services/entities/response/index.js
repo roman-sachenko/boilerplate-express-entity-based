576a816a88c4075bfb380e489345ebfb56154a61
Roman Sachenko <roman.sachenko@gmail.com>
2017-12-07 23:33:35 +0200

    refactor: app error handling refactored

--- app/services/entities/response/index.js
+++ app/services/entities/response/index.js
@@ -9,14 +9,14 @@ const MainService = require('../main');
 
 module.exports = class ResponseService extends MainService {
   constructor() {
-
+    super('Response Service');
   }
 
   static sendSuccessResponse(response, data) {
     response.send({ success: true, data: data })
   }
   static sendErrorResponse(response, err) {
-    let errStatus = err.status || 500;
-    response.status(errStatus).send({ success: false, error: err });
+    const errStatus = err.status || 500;
+    response.status(errStatus).send({ success: false, error: err.message });
   }
 }
