5b983ab3d1cc97ca2f9dabdd9e37f40b2b9a6d0e
Roman Sachenko <roman.sachenko@gmail.com>
2017-12-08 12:03:07 +0200

    feat: entity loaders service added

--- app/services/index.js
+++ app/services/index.js
@@ -6,6 +6,7 @@ module.exports = {
   AUTH            : require(`./${SERVICES_FOLDER_NAME}/auth`),
   USER            : require(`./${SERVICES_FOLDER_NAME}/user`),
   DB_SERVICE      : require(`./${SERVICES_FOLDER_NAME}/db`),
+  ENTITY_LOADER   : require(`./${SERVICES_FOLDER_NAME}/entityLoader`),
   RESPONSE        : require(`./${SERVICES_FOLDER_NAME}/response`),
   LOGGER          : require(`./${SERVICES_FOLDER_NAME}/logger`),
   CRYPTO          : require(`./${SERVICES_FOLDER_NAME}/crypto`)
