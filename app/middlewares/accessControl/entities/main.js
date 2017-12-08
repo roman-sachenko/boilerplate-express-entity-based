013fe84e718efc41e0b31a0145f2479fd6723c2a
Roman Sachenko <roman.sachenko@gmail.com>
2017-12-08 18:33:25 +0200

    feat: access control main handler added

---
+++ app/middlewares/accessControl/entities/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { DbService } = require(`${basePath}/app/services`);
+const UserModel     = DbService.models().User;
+
+module.exports = {
+  isAdmin: (user) => {
+    return !!(user && user.role && user.role === UserModel.ROLES.ADMIN);
+  }
+};
