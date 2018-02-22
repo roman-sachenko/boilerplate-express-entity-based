58fe55ad45d65d2f1eac224ea36141ad762ae26e
Roman Sachenko <roman.sachenko@gmail.com>
2018-02-22 17:11:32 +0200

    refactor: routes architecture refactored

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,21 +1,6 @@
 const express   = require('express');
 const appRoute  = express.Router({ strict: true });
 
-/**
- * Main App Routes (Dummy)
- */
-appRoute.use('/', require('./main.routes'));
-/**
- * Auth Routes
- */
-appRoute.use('/auth', require('./auth.routes/auth.main.routes'));
-appRoute.use('/auth', require('./auth.routes/auth.token.routes'));
-// appRoute.use('/auth', require('./auth.routes/auth.password.routes'));
-/**
- * User Routes
- */
-appRoute.use('/users', require('./user.routes/user.main.routes'));
-appRoute.use('/users', require('./user.routes/user.password.routes'));
-
+appRoute.use('/v1', require('./index.routes'));
 
 module.exports = appRoute;
