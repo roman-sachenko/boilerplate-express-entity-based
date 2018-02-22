4763d06bb64d5e6c5d47d8f3ff2b13e470f9bb65
Roman Sachenko <roman.sachenko@gmail.com>
2018-02-22 17:57:01 +0200

    refactor: config refactored

---
+++ config/env/env.local.js
@@ -0,0 +1,20 @@
+module.exports = {
+  NODE_ENV: '',
+  ENV_PORT: 3000,
+  NODE_INSTANCES_COUNT: 1,
+
+  ADMIN_EMAIL: 'admin@domain.com',
+  ADMIN_PASSWORD: 'test123',
+  ADMIN_FIRST_NAME: 'Admin',
+  ADMIN_LAST_NAME: 'Admin',
+
+  APP_LOGGER_ENABLED: 0,
+
+  JWT_SECRET: 'take.a.look.to.the.sky.just.before.you.die',
+
+  DB_CONNECTION_STRING: 'mongodb://mongo_user:mongo_pass@mongo_server/mongo_db',
+
+  AWS_REGION: 'us-west-2',
+  AWS_ACCESS_KEY_ID: 'yourKey',
+  AWS_SECRET_ACCESS_KEY: 'yourSecret',
+};
