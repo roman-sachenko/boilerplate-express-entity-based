c260bfe2070ad5fc49acfea2f0822a15f0dcdf2e
Roman Sachenko <roman.sachenko@gmail.com>
2018-06-05 13:01:52 +0300

    chore: console log removed

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -2,7 +2,6 @@ const assert = require('assert');
 // const chai = require('chai');
 
 describe('Unit Test Init', () => {
-  console.log('HERE');
   it('First Test', () => {
     assert.equal(undefined);
   });
